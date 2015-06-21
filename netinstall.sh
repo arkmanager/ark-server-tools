fb2110eb2433bcf600ba8a128416dc90722d5b9f
FezVrasta <fez@tekhy.net>
2015-06-21 14:09:51 +0200

    install and netinstall can now update existing installation

--- netinstall.sh
+++ netinstall.sh
@@ -12,13 +12,24 @@ tar -zxvf master.tar.gz
 # Install ARK Server Tools
 cd ark-server-tools-master/tools
 chmod +x install.sh
-if [[ -n $(sh install.sh $1) ]]; then
-  echo "Something where wrong :("
-else
-  echo "ARK Server Tools were correctly installed in your system inside the home directory of $1!"
-fi
+sh install.sh $1
+
+status = $?
 
 # Remove the installation files
 rm -f master.tar.gz
 rm -rf /tmp/ark-server-tools-master
 
+
+if [ $status == 0 ]; then
+  echo "ARK Server Tools were correctly installed in your system inside the home directory of $1!"
+fi
+
+if [ $status == 1 ]; then
+  echo "Something where wrong :("
+fi
+
+if [ $status == 2 ]; then
+  echo "WARNING: A previous version of ARK Server Tools was detected in your system, your old configuration was not overwritten. You may need to manually update it."
+  echo "ARK Server Tools were correctly installed in your system inside the home directory of $1!"
+fi
