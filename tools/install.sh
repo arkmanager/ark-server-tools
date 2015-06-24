80fd03809443cd2905834afcd3f67997712ffcb7
Ben Peddell <klightspeed@killerwolves.net>
2015-06-24 13:20:39 +0000

    Fix typo in init script modification

--- tools/install.sh
+++ tools/install.sh
@@ -30,7 +30,7 @@ if [ ! -z "$1" ]; then
     elif [ -f /etc/rc.d/init.d/functions ]; then
       cp redhat/arkdaemon "${INSTALL_ROOT}/etc/rc.d/init.d/arkmanager"
       chmod +x "${INSTALL_ROOT}/etc/rc.d/init.d/arkmanager"
-      sed -i "s@^DAEMON=\"/usr/bin@DAEMON=\"${EXECPREFIX}@" "${INSTALL_ROOT}/etc/rc.d/init.d/arkmanager"
+      sed -i "s@^DAEMON=\"/usr@DAEMON=\"${EXECPREFIX}@" "${INSTALL_ROOT}/etc/rc.d/init.d/arkmanager"
       if [ -x /sbin/chkconfig -a -z "${INSTALL_ROOT}" ]; then
         chkconfig --add arkmanager
         echo "Ark server will now start on boot, if you want to remove this feature run the following line"
@@ -39,7 +39,7 @@ if [ ! -z "$1" ]; then
     elif [ -f /sbin/runscript ]; then
       cp openrc/arkdaemon "${INSTALL_ROOT}/etc/init.d/arkmanager"
       chmod +x "${INSTALL_ROOT}/etc/init.d/arkmanager"
-      sed -i "s@^DAEMON=\"/usr/bin@DAEMON=\"${EXECPREFIX}@" "${INSTALL_ROOT}/etc/init.d/arkmanager"
+      sed -i "s@^DAEMON=\"/usr@DAEMON=\"${EXECPREFIX}@" "${INSTALL_ROOT}/etc/init.d/arkmanager"
       if [ -x /sbin/rc-update -a -z "${INSTALL_ROOT}" ]; then
         rc-update add arkmanager default
         echo "Ark server will now start on boot, if you want to remove this feature run the following line"
