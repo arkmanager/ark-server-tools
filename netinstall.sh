201c8879d6fbf8e777767bfee806a4fa5bb636a5
FezVrasta <fez@tekhy.net>
2015-06-25 12:56:07 +0200

    added upgrade command, added support for release channels

--- netinstall.sh
+++ netinstall.sh
@@ -4,21 +4,23 @@
 # Net Installer, used with curl
 #
 
+channel=${2:-master} # if defined by 2nd argument install the defined version, otherwise install master
+
 # Download and untar installation files
 cd /tmp
-wget https://github.com/FezVrasta/ark-server-tools/archive/master.tar.gz
-tar -zxvf master.tar.gz
+wget https://github.com/FezVrasta/ark-server-tools/archive/${channel}.tar.gz
+tar -zxvf ${channel}.tar.gz
 
 # Install ARK Server Tools
-cd ark-server-tools-master/tools
+cd ark-server-tools-${channel}/tools
 chmod +x install.sh
 sh install.sh $1 > /dev/null
 
 status=$?
 
 # Remove the installation files
-rm -f master.tar.gz
-rm -rf /tmp/ark-server-tools-master
+rm -f ${channel}.tar.gz
+rm -rf /tmp/ark-server-tools-${channel}
 
 # Print messages
 case "$status" in
