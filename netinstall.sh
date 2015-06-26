fe06479bc906493ee0fe14dab225d3b7ee6ce0c3
FezVrasta <fez@tekhy.net>
2015-06-26 15:58:45 +0200

    ignore cache

--- netinstall.sh
+++ netinstall.sh
@@ -6,7 +6,7 @@
 
 # Download and untar installation files
 cd /tmp
-wget https://github.com/FezVrasta/ark-server-tools/archive/master.tar.gz
+wget --no-cache https://github.com/FezVrasta/ark-server-tools/archive/master.tar.gz
 tar -zxvf master.tar.gz
 
 # Install ARK Server Tools
