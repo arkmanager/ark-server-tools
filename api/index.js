6d124f3761aa74e14adc806da47ff00c64b37cd1
FezVrasta <fez@tekhy.net>
2015-12-22 15:45:13 +0100

    added simple RESTful server to serve API to HTTP

---
+++ api/index.js
@@ -0,0 +1,46 @@
+// import system dependencies to run local commands
+var exec        = require('child_process').exec;
+
+// import rest server module
+var restify     = require('restify');
+
+// generate the server and configure it
+var server = restify.createServer({
+    name: 'ARK Server Tools API',
+    version: '1.4.0'
+});
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+
+// use this function to run a command to the host server and retrieve the stdout and stderr
+function execute(command, callback){
+    exec(command, function(error, stdout, stderr){ callback(stdout, stderr); });
+}
+
+// ========================================================== //
+// define here the functions used to interact with the server //
+// ========================================================== //
+
+function status() {
+    execute('arkmanager status', function(stdout, stderr) {
+        return stdout;
+    });
+}
+
+// ============================================ //
+// define here the routes                       //
+// doc at https://www.npmjs.com/package/restify //
+// ============================================ //
+
+server.get('/status', function(req, res, next) {
+    console.log(status());
+    res.send(JSON.parse(status()));
+});
+
+
+// start the REST server
+// it will listen on port 3100
+server.listen(3100, function () {
+    console.log('%s listening at %s', server.name, server.url);
+});
